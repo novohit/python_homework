9cc6ef4cfe07a1bcaccf9e80f51393a82803a4ac
zwx <weixiong_zhu@163.com>
2022-06-17 16:30:14 +0800

    fix: del

--- 列表/奇特的四位数.py
+++
@@ -1,8 +0,0 @@
-res = []
-for i in range(10**3, 10**4):
-    if i % 11 == 0:
-        num = str(i)
-        if (int(num[0]) + int(num[1]) + int(num[2]) + int(num[3]) == 6) and (num[0] != num[1] != num[2] != num[3]):
-            res.append(i)
-print(len(res))
-print(res)
